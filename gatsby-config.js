23f2d4b92a354d617ffefd9e0afc635540932373
peterwiebe <pjwiebe@gmail.com>
2019-05-08 09:28:36 -0700

    Add contentful source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `CW Blog`,
+    description: `Sharing thoughts`,
+    author: `Peter Wiebe`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +30,16 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,60 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const { edges } = data.allContentfulBlogPost
+  const posts = edges.map(
+    ({
+      node: {
+        id,
+        title,
+        body: {
+          // eslint-disable-next-line no-useless-rename
+          childMarkdownRemark: { excerpt: excerpt },
+        },
+      },
+    }) => (
+      <div key={id}>
+        <h2>{title}</h2>
+        <p>{excerpt}</p>
+      </div>
+    )
+  )
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Blog.</p>
+      {posts}
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  {
+    allContentfulBlogPost {
+      edges {
+        node {
+          id
+          title
+          body {
+            childMarkdownRemark {
+              excerpt(pruneLength: 400)
+            }
+          }
+        }
+      }
+    }
+  }
+`
